60fdd75f814aa896240b0a8db34329f3aba49336
NaimBiswas <nayeembiswas2@gmail.com>
2022-12-11 14:33:56 +0600

    HOME PAGE READY WITH Reused component and with Assests

---
+++ src/app/shared/category-slider/category-slider.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+@Component({
+  selector: 'app-category-slider',
+  templateUrl: './category-slider.component.html',
+  styleUrls: ['./category-slider.component.scss']
+})
+export class CategorySliderComponent implements OnInit {
+
+  constructor() { }
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 0,
+    items: 4,
+    dots: false,
+    nav: true,
+    navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 1200,
+    autoHeight: false,
+    autoplay: true,
+    responsive: {
+
+      0: {
+        items: 1,
+      },
+
+      480: {
+        items: 2,
+      },
+
+      768: {
+        items: 3,
+      },
+
+      992: {
+        items: 4,
+      }
+    }
+  };
+  ngOnInit(): void {
+    $('.set-bg').each(function () {
+      var bg = $(this).data('setbg');
+      $(this).css('background-image', 'url(' + bg + ')');
+    });
+  }
+
+
+}
--- src/app/shared/product-slider/product-slider.component.ts
+++ src/app/shared/product-slider/product-slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OwlOptions, } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-product-slider',
   templateUrl: './product-slider.component.html',
@@ -8,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ProductSliderComponent implements OnInit {
 
   constructor() { }
-
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 0,
+    items: 1,
+    dots: false,
+    nav: false,
+    navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+    smartSpeed: 1200,
+    autoHeight: false,
+    autoplay: true
+  };
   ngOnInit(): void {
   }
 
